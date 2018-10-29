fbcf56aa2c8d1cf795c473d3e7415e525ebdb63e
hubert <hubert.skuza2011@gmail.com>
2018-10-29 20:19:05 +0100

    request, lists, basket

---
+++ public/script.js
@@ -0,0 +1,57 @@
+const PRICE = 9.99;
+
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [],
+        cart: [],
+        newSearch: '',
+        lastSearch: ''
+    },
+    methods: {
+
+        onSubmit() {
+            this.$http.get(`/search/${this.newSearch}`)
+                .then(res => {
+                    this.lastSearch = this.newSearch;
+                    this.items = res.data;
+                })
+        },
+
+        addItem(index) {
+            this.total += PRICE;
+            const pos = this.cart.find(item => item.title == this.items[index].title);
+
+            if (pos) {
+                pos.qty += 1
+            } else {
+                this.cart.push({
+                    id: index,
+                    title: this.items[index].title,
+                    qty: 1,
+                    price: PRICE
+                });
+            }
+        },
+
+
+        inc(item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+
+
+        dec(item) {
+            item.qty--;
+            this.total -= PRICE;
+            const updatingBasket = (item.qty == 0) ? this.cart.splice(this.cart.indexOf(item), 1) : false;
+        }
+    },
+
+    filters: {
+        currency(price) {
+            return `$ ${price.toFixed(2)}`;
+        }
+    }
+});
